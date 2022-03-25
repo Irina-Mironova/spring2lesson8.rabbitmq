b43a1c64a3cf60aff2b897ac4ddcc69832b25039
Ирина <irina250282@mail.ru>
2022-03-25 11:35:51 +0500

    Init

---
+++ src/main/java/ru/geekbrains/spring2/lesson8/rabbitmq/consumer/ItReceiverApp.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.spring2.lesson8.rabbitmq.consumer;
+
+public class ItReceiverApp {
+}
---
+++ src/main/java/ru/geekbrains/spring2/lesson8/rabbitmq/producer/ItSenderApp.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.spring2.lesson8.rabbitmq.producer;
+
+import com.rabbitmq.client.ConnectionFactory;
+
+public class ItSenderApp {
+    public static void main(String[] args) {
+    }
+}
